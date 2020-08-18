b04a8a58ca38815e8ded3d11816ad59c45fc0350
Desiree Gamez <desireegamezinfantes@gmail.com>
2020-08-18 19:31:08 +0200

    últimos cambios en el ejercicio

--- classMobile.ts
+++ classMobile.ts
@@ -4,14 +4,14 @@ export
 class Mobile
 {
     //Declaración de atributos o propiedades
-    public name: string;
-    public model: string;
-    public trademark: string; 
-    public sdSize: number;
-    public color: string;
-    public is5G: boolean;
-    public cameraNumber: number;
-    public price: number; 
+    private name: string;
+    private model: string;
+    private trademark: string; 
+    private sdSize: number;
+    private color: string;
+    private is5G: boolean;
+    private cameraNumber: number;
+    private price: number; 
 
     //Implementación del método constructor
     constructor (name:string, model: string, trademark: string, sdSize: number, color:string, is5G:boolean, cameraNumber: number, price: number)
@@ -25,6 +25,93 @@ class Mobile
         this.cameraNumber = cameraNumber;
         this.price = price; 
     }
+
+    //Métodos setters y getters
+    setName(name: string) 
+    {
+        this.name = name;
+    }
+
+    getName():string 
+    {
+        return this.name;
+    }
+
+    setModel(model: string) 
+    {
+        this.model = model;
+    }
+
+    getModel():string 
+    {
+        return this.model;
+    }
+
+    setTrademark(trademark: string) 
+    {
+        this.trademark = name;
+    }
+
+    getTrademark():string 
+    {
+        return this.trademark;
+    }
+
+    setSdSize(sdSize: number) 
+    {
+        this.sdSize = sdSize;
+    }
+
+    getSdSize():number 
+    {
+        return this.sdSize;
+    }
+
+    setColor(color: string) 
+    {
+        this.color = color;
+    }
+
+    getColor():string 
+    {
+        return this.color;
+    }
+
+    setIs5G(is5G: boolean) 
+    {
+        this.is5G = is5G;
+    }
+
+    getIs5G():boolean 
+    {
+        return this.is5G;
+    }
+
+    setCameraNumber(cameraNumber: number) 
+    {
+        this.cameraNumber = cameraNumber;
+    }
+
+    getCameraNumber():number 
+    {
+        return this.cameraNumber;
+    }
+    
+    setPrice(price: number) 
+    {
+        this.price = price;
+    }
+
+    getPrice():number 
+    {
+        return this.price;
+    }
+
+    public printProperities () //Método que imprime todas las características del objeto en modo lista
+    {
+        console.log("* 'The characteristics of the mobile " + this.name + " are:'" + "\n" + "     -Name: " + this.name + "\n" + "     -Model: " + this.model + "\n" + "     -Trademark: " + this.trademark + "\n" + "     -SD Size (GB): " + this.sdSize + "\n" + "     -Color: " + this.color + "\n" + "     -Is 5g?: " + this.is5G + "\n" + "     -Number of Cameras: " + this.cameraNumber + "\n" + "     -Price: " + this.price);
+    } 
+
 }
 
 
---
+++ newObjects.ts
@@ -0,0 +1,68 @@
+// En otro fichero hacer las mmismas operaciones que se hicieron en el punto 2:
+// 2.1 - Crear un objeto de la clase Mobile llamado Nokia3210, iPhone3G y Samsung Galaxy 10
+
+import { Mobile } from "./classMobile";
+
+let Nokia3210 = new Mobile("Nokia3210", "model3210", "Nokia Corporation", 5, "red", false, 1, 65.00);
+let iPhone3G = new Mobile("Iphone", "Iplus3", "Apple", 128, "silver", true, 3, 899.00);
+let SamsungGalaxy10 = new Mobile("Samsung", "Galaxy10", "SSG Corporation", 64, "black", true, 2, 450.99);
+
+// 2.2 - Mostrar por consola todos los atributos de cada objeto
+console.log(Nokia3210);
+console.log(iPhone3G);
+console.log(SamsungGalaxy10);
+
+//B) Usando el método print creado, que es público y  muestra todo en formato lista
+Nokia3210.printProperities();  
+iPhone3G.printProperities();
+SamsungGalaxy10.printProperities();
+
+//C) opcion mega pro de developer que crea getters y luego, además, LOS USA para mostrar atributos ahora privados uno a uno
+console.log(Nokia3210.getName());
+console.log(Nokia3210.getModel());
+console.log(Nokia3210.getTrademark());
+console.log(Nokia3210.getSdSize());
+console.log(Nokia3210.getColor());
+console.log(Nokia3210.getIs5G());
+console.log(Nokia3210.getCameraNumber());
+console.log(Nokia3210.getPrice());
+
+// 2.3 - Modificar el Nokia3210 para que tenga 5G y 4 camaras
+Nokia3210.setIs5G(true);
+Nokia3210.setCameraNumber(4);
+Nokia3210.printProperities(); // para comprobar que los valores cambian
+
+// 2.4 - Mostrar todos los atributos de todos los objetos creados
+//Be getter my friend: 
+
+console.log(iPhone3G.getName());
+console.log(iPhone3G.getModel());
+console.log(iPhone3G.getTrademark());
+console.log(iPhone3G.getSdSize());
+console.log(iPhone3G.getColor());
+console.log(iPhone3G.getIs5G());
+console.log(iPhone3G.getCameraNumber());
+console.log(iPhone3G.getPrice());
+
+
+console.log(SamsungGalaxy10.getName());
+console.log(SamsungGalaxy10.getModel());
+console.log(SamsungGalaxy10.getTrademark());
+console.log(SamsungGalaxy10.getSdSize());
+console.log(SamsungGalaxy10.getColor());
+console.log(SamsungGalaxy10.getIs5G());
+console.log(SamsungGalaxy10.getCameraNumber());
+console.log(SamsungGalaxy10.getPrice());
+
+// Crear un array de objetos de la clase Mobile con los tres objetos ya creados. 
+let miLocutorio = new Array();
+miLocutorio = [Nokia3210, iPhone3G, SamsungGalaxy10];
+
+// Mostrar cada uno de los atributos de cada unos de los objetos del array creado en modo lista.
+for (let i=0; i <miLocutorio.length; i++)
+{
+    console.log ("* 'The characteristics of the mobile " + miLocutorio[i].getName() + " are:'" + "\n" + "     -Name: " + miLocutorio[i].getName() 
+    + "\n" + "     -Model: " + this.model + "\n" + "     -Trademark: " + this.trademark + "\n" + "     -SD Size (GB): " + this.sdSize + "\n" +
+     "     -Color: " + this.color + "\n" + "     -Is 5g?: " + this.is5G + "\n" + "     -Number of Cameras: " + this.cameraNumber + "\n" + 
+     "     -Price: " + this.price + "\n");
+}
